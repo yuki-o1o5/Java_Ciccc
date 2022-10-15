efaa922c67f6b8cb593d1fe7b26e0688ad1f9135
yuki-o1o5 <jkksst1712@gmail.com>
2022-10-15 15:30:37 -0700

    initial commit

---
+++ Week2AnimalClassAssignment/src/Animal/Animaal.java
@@ -0,0 +1,14 @@
+package Animal;
+
+public class Animaal {
+  private int height;
+  private int weight;
+  private String animaltype;
+  private String bloodtyoe;
+  
+  public Animal(int height, int weight, String animaltype, String bloodtyoe) {
+    this.height = height;
+    this.weight = weight;
+    this.animaltype = animaltype;
+    this.bloodtyoe = bloodtyoe;
+}
