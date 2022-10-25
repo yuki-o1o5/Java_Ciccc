27b8dd1cd2d53a743c6eae5510eac394691b4e78
yuki-o1o5 <jkksst1712@gmail.com>
2022-10-24 22:59:07 -0700

    make note feature

---
+++ Week4JavaMidtermProject/src/App.java
@@ -0,0 +1,19 @@
+import java.util.spi.TimeZoneNameProvider;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        
+// Billing System ・Order cafe latte
+
+// 1 Cafe latte(hot, ice)
+// 2 Choose size (small, medium, Large)
+// 3 Choose espresso (regular or decaf)
+// 4 Chose shot (regular or extra）
+// 5 Choose milk (regular milk, oats milk, almond milk)
+// 6 Syrup (none, caramel, vanilla, hazelnuts)
+// 7 for here or to go
+// 8 Tip (10%, 15%, 20%)
+// 9 Tax
+    }
+}
