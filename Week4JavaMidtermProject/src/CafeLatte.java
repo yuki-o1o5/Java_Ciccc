a6e97c1f4ef03dd4bcfbfca67cc0906792885103
yuki-o1o5 <jkksst1712@gmail.com>
2022-10-28 10:39:49 -0700

    add 2 loops

--- Week4JavaMidtermProject/src/CafeLatte.java
+++ Week4JavaMidtermProject/src/CafeLatte.java
@@ -3,73 +3,185 @@ import java.util.Scanner;
 public class CafeLatte {
   
   public static void main (String[] args){
-    int userChoice =0;
-    int number1 = 1;
-    int number2 = 2;
-    int output =0;
-    String yesNo = "yes";
-  
+    int menu = 1;
+    int typeOfEspresso = 1;
+    int numberOfShot = 1;
+    int milkChoice = 1;
+    int syrupChoice = 1;
+    int hereToGo = 1;
+    double output = 0;
 
-  Scanner sc = new Scanner(System.in)
+    String yesNo = "yes";
+    Scanner sc = new Scanner(System.in);
 
   try {
 
-    while(true){
+    while(yesNo.equals("yes")){
 
-      System.out.println("============== Menu ==============");
+      printMessage("Menu");
+      // System.out.println("============== Menu ==============");
       System.out.println("1: Small cafe latte       /$4.45");
-      System.out.println("2: Small Ice cafe latte   /$4.45");
-      System.out.println("3: Medium cafe latte      /$4.95");
-      System.out.println("4: Medium Ice cafe latte  /$4.95");
-      System.out.println("5: Large cafe latte       /$5.45");
-      System.out.println("6: Large Ice cafe latte   /$5.45");
+      System.out.println("2: Medium cafe latte      /$4.95");
+      System.out.println("3: Large cafe latte       /$5.45");
       System.out.println("==================================");
       System.out.println("PLease enter your choice : ");
-      userChoice = sc.nextInt();
+      menu = sc.nextInt();
 
-      System.out.println("======== Type of Espresso ========");
+      printMessage("Type of Espresso");
+      // System.out.println("======== Type of Espresso ========");
       System.out.println("1: Regular  /Free");
       System.out.println("2: Decaf    /Free");
       System.out.println("==================================");
       System.out.println("Which shots would you like?");
-      userChoice = sc.nextInt();
+      typeOfEspresso = sc.nextInt();
 
-      System.out.println("========= Number of Shot =========");
+      printMessage("Number of Shot");
+      // System.out.println("========= Number of Shot =========");
       System.out.println("1: Double Shot        / Free");
       System.out.println("2: Single Shot        /-$0.5");
       System.out.println("3: Extra Single Shot  /+$0.6");
       System.out.println("4: Extra Double Shot  /+$1.25");
       System.out.println("==================================");
       System.out.println("How many shots would you like?");
-      userChoice = sc.nextInt();
+      numberOfShot = sc.nextInt();
 
-      System.out.println("=========== Milk Choice ===========");
+      printMessage("Milk Choice");
+      // System.out.println("=========== Milk Choice ===========");
       System.out.println("1: Regular milk  /Free");
-      System.out.println("2: Skim milk     /Free");
-      System.out.println("3: Lactose milk  /Free");
-      System.out.println("4: Oat milk      /+$0.8");
-      System.out.println("5: Almond milk   /+$0.8");
+      System.out.println("2: Oat milk      /+$0.8");
+      System.out.println("3: Almond milk   /+$0.8");
       System.out.println("===================================");
       System.out.println("Which milk would you like?");
-      userChoice = sc.nextInt();
+      milkChoice = sc.nextInt();
 
-      System.out.println("=========== Syrup Choice ===========");
+      printMessage("Syrup Choice");
+      // System.out.println("=========== Syrup Choice ===========");
       System.out.println("1: None        /Free");
-      System.out.println("2: Half Sweet  /Free");
-      System.out.println("3: vanilla     /+$0.5");
-      System.out.println("4: caramel     /+$0.5");
-      System.out.println("5: hazelnut    /+$0.5");
+      System.out.println("2: vanilla     /+$0.5");
+      System.out.println("3: caramel     /+$0.5");
+      System.out.println("4: hazelnut    /+$0.5");
       System.out.println("5: lavender    /+$0.5");
       System.out.println("===================================");
       System.out.println("Would you like add syrup?");
-      userChoice = sc.nextInt();
+      syrupChoice = sc.nextInt();
 
-      System.out.println("========= For here To go =========");
+      printMessage("For here To go");
+      // System.out.println("========= For here To go ==========");
       System.out.println("1: For Here           /Free");
       System.out.println("2: To Go(Cup Charge)  /+$0.25");
       System.out.println("===================================");
-      System.out.println("For here? or To go?");
-      userChoice = sc.nextInt();
+      System.out.println("For here or to go?");
+      hereToGo = sc.nextInt();
+
+      if(menu == 1){
+        output = output + 4.45;
+      }
+
+      if(menu == 2){
+        output = output + 4.95;
+      }
+
+      if(menu == 3){
+        output = output + 5.45;
+      }
+
+      if(typeOfEspresso == 1){
+        output = output + 0;
+      }
+
+      if(typeOfEspresso == 2){
+        output = output + 0;
+      }
+
+      if(numberOfShot == 1){
+        output = output + 0;
+      }
+
+      if(numberOfShot == 2){
+        output = output - 0.5;
+      }
+
+      if(numberOfShot == 3){
+        output = output + 0.6;
+      }
+
+      if(numberOfShot == 4){
+        output = output + 1.25;
+      }
+
+      if(milkChoice == 1){
+        output = output + 0;
+      }
+      
+      if(milkChoice == 2){
+        output = output + 0.8;
+      }
+
+      if(milkChoice == 3){
+        output = output + 0.8;
+      }
+
+      if(syrupChoice == 1){
+        output = output + 0;
+      }
+
+      if(syrupChoice == 2){
+        output = output + 0.5;
+      }
+
+      if(syrupChoice == 3){
+        output = output + 0.5;
+      }
+
+      if(syrupChoice == 4){
+        output = output + 0.5;
+      }
+
+      if(syrupChoice == 5){
+        output = output + 0.5;
+      }
+
+      if(hereToGo == 1){
+        output = output + 0;
+      }
+
+      if(hereToGo == 2){
+        output = output + 0.25;
+      }
+      
+      System.out.println("Total cost is : " + output);
+
+      while(yesNo.equals("yes")){
+        System.out.println("Do you want to order another one?  (yes or no): ");
+        yesNo = sc.next();
+
+        if(yesNo.equals("yes")){
+          break;
+        }
+
+        else if(yesNo.equals("no")){
+          System.out.println("Do you want to pay tax?");
+          break;
+        }
+
+        else{
+          System.out.println("Prease enter yes or no.");
+          yesNo = "yes";
+      }
+
+
+
+    }
+  } finally{
+
+  }
+}
+
+public static void printMessage(String input) {
+  System.out.println("========= " + input + " =========");
+}
+}
+ 
 
 // 7 add more order or Not?
 // 8 Tip (10%, 15%, 20%)
@@ -86,10 +198,3 @@ public class CafeLatte {
 // 7 add more order
 // 8 Tip (10%, 15%, 20%)
 // 9 Tax
-
-    }
-
-      }
-  }
-}
- 
